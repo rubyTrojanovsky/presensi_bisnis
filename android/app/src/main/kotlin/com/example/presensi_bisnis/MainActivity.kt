6f44e8203651aad001e2de62ec2ff4ec7fc86323
Ruby Trojanovsky <armymadesu@gmail.com>
2022-10-14 10:09:50 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/presensi_bisnis/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.presensi_bisnis
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/scan_qr.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ScanQR extends StatelessWidget {
+  const ScanQR({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
