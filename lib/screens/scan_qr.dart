9cf36405a919371461b642dfa6d13d37e842e13e
Ruby Trojanovsky <armymadesu@gmail.com>
2022-10-24 11:05:55 +0700

    upgrade flutter + scan QR

--- lib/screens/scan_qr.dart
+++ lib/screens/scan_qr.dart
@@ -1,25 +1,86 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:presensi_bisnis/controller/absen_controller.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 
-class ScanQR extends StatelessWidget {
-  const ScanQR({ Key? key }) : super(key: key);
+class ScanQR extends StatefulWidget {
+  ScanQR({ Key? key }) : super(key: key);
+
+  @override
+  State<ScanQR> createState() => _ScanQRState();
+}
+
+class _ScanQRState extends State<ScanQR> {
+  final qrKey = GlobalKey(debugLabel: 'QR');
+  Barcode? result;
+  QRViewController? controller;
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller!.pauseCamera();
+    } else if (Platform.isIOS) {
+      controller!.resumeCamera();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     AbsenController controller = Get.put(AbsenController());
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(16, 48, 16, 18),
-        child: Scaffold(
-          body: Column(
-            children: [
-              Text("matkul " + controller.matkul.string),
-              Text("tanggal " + controller.tanggal.string)
-            ],
+      body: Container(
+        child: SingleChildScrollView(
+          child: Column(
+              children: [
+                // Text("matkul " + controller.matkul.string),
+                // Text("tanggal " + controller.tanggal.string),
+                SizedBox(
+                  height: size.height * 0.6,
+                  child: buildQrView(context),
+                ),
+                Center(
+              child: (result != null)
+                  ? Text(
+                      'Barcode Type: ${describeEnum(result!.format)}   Data: ${result!.code}')
+                  : Text('Scan a code'),
+            ),
+                
+              ],
+            ),
           ),
-        ),
       ),
+      
     );
+  }
+
+  Widget buildQrView(BuildContext) => QRView(
+    key: qrKey,
+    onQRViewCreated: _onQRViewCreated,
+    overlay: QrScannerOverlayShape(),);
+
+  void onQRViewCreated(QRViewController controller){
+    setState(() =>
+      this.controller = controller
+    ); 
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        result = scanData;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
   }
 }
