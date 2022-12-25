1b00f8a6419c69fa7024a12cecf880198ee02ac8
Ruby Trojanovsky <armymadesu@gmail.com>
2022-12-25 10:28:14 +0700

    daftar hadir done

--- lib/screens/daftar_hadir.dart
+++ lib/screens/daftar_hadir.dart
@@ -19,16 +19,33 @@ class DaftarHadir extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Color.fromARGB(255, 29, 133, 69)),
       body: Padding(
-        padding: const EdgeInsets.fromLTRB(16, 48, 16, 18),
+        padding: const EdgeInsets.fromLTRB(16, 30, 16, 0),
         child: Container(
-          height: size.height *0.8,
+          height: size.height *0.85,
           width: size.width,
           child: Column(
             children: [
-              Text("Mata kuliah: " + controller.matkul.value),
-              Text("Minggu ke-" + controller.tanggal.value.toString()),
+              Text("Mata kuliah: " + controller.matkul.value,
+              style: TextStyle(
+                                  fontSize: 18
+                                ),),
+              SizedBox(
+                height: 4,
+              ),
+              Text("Minggu ke-" + controller.tanggal.value.toString(),
+              style: TextStyle(
+                                  fontSize: 18
+                                ),),
+              SizedBox(
+                height: 16,
+              ),
               Container(
-                height: size.height *0.7,
+                padding: EdgeInsets.all(10.0),
+                height: size.height *0.75,
+                width: size.width * 0.85,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black)
+                ),
                 child: FutureBuilder<DaftarHadirModel>(future: api.getDaftarHadir(),
             builder: (context, snapshot) {
                 if (snapshot.hasData) {
@@ -36,18 +53,21 @@ class DaftarHadir extends StatelessWidget {
                     itemCount: snapshot.data!.mahasiswa!.length,
                     itemBuilder: (context, index) {
                       return Container(
-                        height: size.height*0.025,
+                        height: size.height*0.03,
                         child: Column(
                           children: [
                             Row(
                               children: [
-                                Text(snapshot.data!.mahasiswa![index].npm.toString()),
+                                Text(snapshot.data!.mahasiswa![index].npm.toString(),
+                                ),
                                 SizedBox(
                                   width: 10,
                                 ),
                                 Text(snapshot.data!.mahasiswa![index].nama.toString()),
                               ],
-                            )
+                            ),
+                            SizedBox(height: 5,),
+                            Divider(height: 1, thickness: 1, color: Colors.black,)
                           ],
 
                         ),
